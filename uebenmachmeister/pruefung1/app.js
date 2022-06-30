a3155db907a9f41b073626f9397fc3ae4becd101
shamel jij <shamel.jij@gmail.com>
2022-06-30 13:48:22 +0200

    more files

---
+++ uebenmachmeister/pruefung1/app.js
@@ -0,0 +1,54 @@
+function antwort1() {
+  var text1 = document.getElementById("antwort1").className;
+  if (text1 == "d-none") {
+    document.getElementById("antwort1").className = "";
+    document.getElementById("antwort1").className += "d-block";
+  } else if (text1 == "d-block") {
+    document.getElementById("antwort1").className = "";
+    document.getElementById("antwort1").className += "d-none";
+  }
+}
+
+function antwort2() {
+  var text2 = document.getElementById("antwort2").className;
+  if (text2 == "d-none") {
+    document.getElementById("antwort2").className = "";
+    document.getElementById("antwort2").className += "d-block";
+  } else if (text2 == "d-block") {
+    document.getElementById("antwort2").className = "";
+    document.getElementById("antwort2").className += "d-none";
+  }
+}
+
+function antwort3() {
+  var text3 = document.getElementById("antwort3").className;
+  if (text3 == "d-none") {
+    document.getElementById("antwort3").className = "";
+    document.getElementById("antwort3").className += "d-block";
+  } else if (text3 == "d-block") {
+    document.getElementById("antwort3").className = "";
+    document.getElementById("antwort3").className += "d-none";
+  }
+}
+
+function antwort4() {
+  var text4 = document.getElementById("antwort4").className;
+  if (text4 == "d-none") {
+    document.getElementById("antwort4").className = "";
+    document.getElementById("antwort4").className += "d-block";
+  } else if (text4 == "d-block") {
+    document.getElementById("antwort4").className = "";
+    document.getElementById("antwort4").className += "d-none";
+  }
+}
+
+function antwort5() {
+  var text5 = document.getElementById("antwort5").className;
+  if (text5 == "d-none") {
+    document.getElementById("antwort5").className = "";
+    document.getElementById("antwort5").className += "d-block";
+  } else if (text5 == "d-block") {
+    document.getElementById("antwort5").className = "";
+    document.getElementById("antwort5").className += "d-none";
+  }
+}
