682f832cffcddabacbf6a21cb92e0378d14b5f89
shameljij <shamel.jij@gmail.com>
2022-06-30 17:57:15 +0200

    font size on print changed

--- uebenmachmeister/pruefung1/app.js
+++ uebenmachmeister/pruefung1/app.js
@@ -1,3 +1,25 @@
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 function antwort1() {
   var text1 = document.getElementById("antwort1").className;
   if (text1 == "d-none") {
