3a762abf5053ceb1686c6bc4ae89b2ca9b936750
shamel jij <shamel.jij@gmail.com>
2022-07-01 12:11:34 +0200

    hsa22 added

--- uebenmachmeister/pruefung2/app.js
+++ uebenmachmeister/pruefung2/app.js
@@ -19,56 +19,67 @@ function topFunction() {
 }
 
 function antwort1() {
-    var text1 = document.getElementById("antwort1").className;
+    var text1 = document.getElementById("hsa21").className;
     if (text1 == "d-none") {
-      document.getElementById("antwort1").className = "";
-      document.getElementById("antwort1").className += "d-block";
+      document.getElementById("hsa21").className = "";
+      document.getElementById("hsa21").className += "d-block";
     } else if (text1 == "d-block") {
-      document.getElementById("antwort1").className = "";
-      document.getElementById("antwort1").className += "d-none";
+      document.getElementById("hsa21").className = "";
+      document.getElementById("hsa21").className += "d-none";
     }
-  }
-  
-  function antwort2() {
-    var text2 = document.getElementById("antwort2").className;
+}
+
+function antwort2a() {
+    var text2 = document.getElementById("hsa22a").className;
     if (text2 == "d-none") {
-      document.getElementById("antwort2").className = "";
-      document.getElementById("antwort2").className += "d-block";
+        document.getElementById("hsa22a").className = "";
+        document.getElementById("hsa22a").className += "d-block";
     } else if (text2 == "d-block") {
-      document.getElementById("antwort2").className = "";
-      document.getElementById("antwort2").className += "d-none";
+        document.getElementById("hsa22a").className = "";
+        document.getElementById("hsa22a").className += "d-none";
     }
-  }
-  
-  function antwort3() {
+}
+
+function antwort2b() {
+    var text2 = document.getElementById("hsa22b").className;
+    if (text2 == "d-none") {
+        document.getElementById("hsa22b").className = "";
+        document.getElementById("hsa22b").className += "d-block";
+    } else if (text2 == "d-block") {
+        document.getElementById("hsa22b").className = "";
+        document.getElementById("hsa22b").className += "d-none";
+    }
+}
+
+function antwort3() {
     var text3 = document.getElementById("antwort3").className;
     if (text3 == "d-none") {
-      document.getElementById("antwort3").className = "";
-      document.getElementById("antwort3").className += "d-block";
+        document.getElementById("antwort3").className = "";
+        document.getElementById("antwort3").className += "d-block";
     } else if (text3 == "d-block") {
-      document.getElementById("antwort3").className = "";
-      document.getElementById("antwort3").className += "d-none";
+        document.getElementById("antwort3").className = "";
+        document.getElementById("antwort3").className += "d-none";
     }
-  }
-  
-  function antwort4() {
+}
+
+function antwort4() {
     var text4 = document.getElementById("antwort4").className;
     if (text4 == "d-none") {
-      document.getElementById("antwort4").className = "";
-      document.getElementById("antwort4").className += "d-block";
+        document.getElementById("antwort4").className = "";
+        document.getElementById("antwort4").className += "d-block";
     } else if (text4 == "d-block") {
-      document.getElementById("antwort4").className = "";
-      document.getElementById("antwort4").className += "d-none";
+        document.getElementById("antwort4").className = "";
+        document.getElementById("antwort4").className += "d-none";
     }
-  }
-  
-  function antwort5() {
+}
+
+function antwort5() {
     var text5 = document.getElementById("antwort5").className;
     if (text5 == "d-none") {
-      document.getElementById("antwort5").className = "";
-      document.getElementById("antwort5").className += "d-block";
+        document.getElementById("antwort5").className = "";
+        document.getElementById("antwort5").className += "d-block";
     } else if (text5 == "d-block") {
-      document.getElementById("antwort5").className = "";
-      document.getElementById("antwort5").className += "d-none";
+        document.getElementById("antwort5").className = "";
+        document.getElementById("antwort5").className += "d-none";
     }
-  }
+}
